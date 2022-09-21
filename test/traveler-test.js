e6725f58ec915f8c81c5c37d9af148cdd9eacb59
Jedeo Manirikumwenatwe <jedeopastor@gmail.com>
2022-09-21 11:49:24 -0600

    Add: test for traveler class

---
+++ test/traveler-test.js
@@ -0,0 +1,23 @@
+import chai from "chai";
+const expect = chai.expect;
+const travelersTestData = require("../src/data/travelersTestData");
+import Traveler from "../src/components/Traveler";
+
+describe("Traveler", function () {
+  let traveler1;
+  beforeEach(() => {
+    traveler1 = new Traveler(travelersTestData[9]);
+  });
+
+  it("should have an id", function () {
+    expect(traveler1.id).to.equal(10);
+  });
+
+  it("should have a name", function () {
+    expect(traveler1.name).to.equal("Rickie Jodlowski");
+  });
+   
+  it("should have a travelerType", function () {
+    expect(traveler1.travelerType).to.equal('relaxer')
+  });
+});
