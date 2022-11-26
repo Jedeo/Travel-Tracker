269165df5dde9a9af32937b1d518787f507a1bea
Jedeo <jedeopastor@gmail.com>
2022-11-26 14:22:33 -0700

    Add: srcrit to vercel

--- src/script-2.js
+++ src/script-2.js
@@ -1,4 +1,4 @@
 import {checkTravelerLogin} from './scripts'
-console.log('hello')
+
 const loginButton = document.querySelector(".login-button");
 loginButton.addEventListener("click", checkTravelerLogin);
